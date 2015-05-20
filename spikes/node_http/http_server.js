337dba9a53ccfd5a45b4c7db663cc7bf4de42b3d
Ariel Zusman <ariel.zusman@gmail.com>
2015-05-20 07:43:26 +0300

    e9 started Http server

---
+++ spikes/node_http/http_server.js
@@ -0,0 +1,16 @@
+/* Very simple node https server*/
+var http = require('http');
+
+var server = http.createServer();
+
+server.on("request", function(request, response){
+	console.log("Recived request");
+
+	var body = "<html><head><title>Node http spike</title></head><body><p>This is a Node HTTP sever</p></body></html>";
+
+	response.end(body);
+});
+
+server.listen(8080);
+
+console.log("server started");
